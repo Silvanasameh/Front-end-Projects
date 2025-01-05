5c8bbfce4c14500a4da007e1002fe44d9fe70044
Silvanasameh <selvanasevo@gmail.com>
2025-01-05 17:50:55 +0200

    Add files via upload

---
+++ html&css projects/savag-brand-project/script.js
@@ -0,0 +1,63 @@
+// JavaScript source code
+
+var imgList = document.getElementById('imgList');
+var scrollRight = document.getElementById('scroll-right');
+var scrollLeft = document.getElementById('scroll-left');
+
+// When a user clicks on the right arrow, the ul will scroll 750px to the right
+scrollRight.addEventListener('click', (event) => {
+    imgList.scrollBy(750, 0);
+});
+
+// When a user clicks on the left arrow, the ul will scroll 750px to the left
+scrollLeft.addEventListener('click', (event) => {
+    imgList.scrollBy(-750, 0);
+});
+filterSelection("all")
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+// Change The Picture and Associated Element Color when Color Options Are Clicked.
